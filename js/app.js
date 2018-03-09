c8baa393c0162e24387192cba902f977d58335be
vinhvu200 <vinhvu200@gmail.com>
2018-03-09 15:26:15 -0800

    Moved navbar to each individual views

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mainModule', ['ngRoute', 'ngAnimate', 'projectsModule', 'contactModule']);
+var app = angular.module('mainModule', ['ngRoute', 'ngAnimate', 'projectsModule', 'contactModule', 'introductionModule']);
 
 app.directive('contact', function(){
 
@@ -10,52 +10,14 @@ app.directive('contact', function(){
 	}
 });
 
-app.service('MainService', function(){
-
-	var introTab = new Tab('Introduction', '#/');
-	introTab.setActive();
-	var projectsTab = new Tab('Projects', '#!projects');
-	var contactTab = new Tab('Contact', '#!contact');
-
-	var mainService = {};
-	mainService.currentTab = introTab.name;
-	mainService.tabs = [contactTab, projectsTab, introTab];
-	
-	mainService.setTab = function(tabNum) {
-
-		if (tabNum === 0){
-			return;
-		}
-
-		var i;
-		for (i=0; i<mainService.tabs.length; i++){
-			mainService.tabs[i].setUnactive();
-		}
-
-		mainService.tabs[tabNum].setActive();
-		mainService.currentTab = mainService.tabs[tabNum].name;
-	}
-
-	return mainService;
-});
-
-app.controller('MainController', function(MainService){
-
-	this.tabs = MainService.tabs;
-	this.currentTab = MainService.currentTab;
-
-	this.setTab = function(tabNum) {
-		MainService.setTab(tabNum);
-		this.currentTab = MainService.currentTab;
-	}
-});
-
 app.config(function($routeProvider){
 
 	$routeProvider
 
 	.when('/', {
-		templateUrl: 'views/introduction.html'
+		templateUrl: 'views/introduction.html',
+		controller: 'IntroductionController',
+		controllerAs: 'intro'
 	})
 
 	.when('/projects', {
